70a234365cd9c01df20b458621f32cd7ba3654eb
Caue Prado <caue.prado0@gmail.com>
2021-03-11 21:58:10 -0300

    Removido os comentários.

--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -2,12 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
-import Http from 'http'
 import logger from '@infrastructure/logger'
 import router from '@infrastructure/router'
 
 export default class Server {
-  private static _instance: Http.Server
   private static appEnv = process.env.NODE_ENV || 'dev'
 
   public static async start(): Promise<express.Express> {
@@ -22,11 +20,6 @@ export default class Server {
       await router.loadRoutes(app)
       app.get('/', (req, res) => res.status(200).send())
 
-      // app.listen(this.serverPort())
-
-      // this._instance = Http.createServer(express)
-
-      // return this._instance
       return app
     } catch (error) {
       logger.error(error.message, error)
@@ -38,10 +31,5 @@ export default class Server {
     return Number.isNaN(Number(process.env.NODE_PORT))
       ? 3500
       : Number(process.env.NODE_PORT)
-  }
-
-  public static close(callback?: () => void): void {
-    logger.info('chamou o close')
-    this._instance.close(callback)
   }
 }
